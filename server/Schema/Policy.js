a30209c699ad0eb703fdd4e4743b44ff46c44f11
SirajAnsari3 <siraj2022ansariAgamil.com>
2025-05-21 17:47:28 +0500

    improved backend adaptability

--- server/Schema/Policy.js
+++ server/Schema/Policy.js
@@ -2,16 +2,17 @@ import mongoose, { Schema } from "mongoose";
 
 
 const policySchema = new Schema({
-  policyNumber:  { type: String, required: true, unique: true },
-  holderName:    { type: String, required: true },
-  nic:           { type: String, required: true, unique: true },
+  policyNumber:  { type: String,   required: true, unique: true },
+  holderName:    { type: String,   required: true },
+  nic:           { type: String,   required: true, unique: false },
   dob:           { type: String,   required: true },
   startDate:     { type: String,   required: true },
   lastPaidDate:  { type: String,   required: true },
-  premium:       { type: Number, required: true },
-  sumAssured:    { type: Number, required: true },
+  premium:       { type: Number,   required: true },
+  sumAssured:    { type: Number,   required: true },
   riders:        { type: [String], default: [] },
-  whatsapp:      { type: String }
+  whatsapp:      { type: String,   required: true },
+  email:         { type: String}
 });
 
 
